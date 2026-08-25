97f8f3249551812fda19b731edf7e3ba8b525ec1
AlichTrZ <felipetro44@gmail.com>
2026-08-25 17:07:58 -0500

    Correción del codigo del primer ejercicio (Se me olvidó poner variables en CamelCase)
    Correccion del primer ejercicio, variables puestas ahora en CamelCase

--- java-incrementos-y-condicionales/src/Programas/Ejercicio2AccesoConjunto.java
+++ java-incrementos-y-condicionales/src/Programas/Ejercicio2AccesoConjunto.java
@@ -61,7 +61,7 @@ public class Ejercicio2AccesoConjunto {
         }
         
             
-              
+   
     }
     
 }
---
+++ java-incrementos-y-condicionales/src/Programas/Ejercicio3Transporte.java
@@ -0,0 +1,38 @@
+/*
+Nivel: Avanzado
+Tipo de estructura: Condiciones compuestas
+Una aplicación de transporte asigna un conductor premium únicamente si:
+• Tiene calificación superior o igual a 4.8.
+• Ha realizado más de 500 viajes.
+• Se encuentra disponible.
+Si cumple dos de las tres condiciones será asignado como conductor estándar.
+En cualquier otro caso no podrá ser asignado.
+Requerimientos
+Solicitar:
+• Calificación.
+• Cantidad de viajes.
+• Disponibilidad.
+Mostrar el tipo de asignación.
+ */
+package Programas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andres Felipe Trochez Lugo
+ */
+public class Ejercicio3Transporte {
+    
+    public static void main(String[] args) {
+        
+        String calificacion;
+        String cantviajes
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("");
+        
+    }
+    
+}
