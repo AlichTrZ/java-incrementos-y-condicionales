06bc5578876e9f6ba77128b1e251fa74f12f48dd
AlichTrZ <felipetro44@gmail.com>
2026-08-25 17:57:37 -0500

    Se añade el ejercicio 3.
    Se sube el codigo y su debida evidencia.

--- java-incrementos-y-condicionales/src/Programas/Ejercicio3Transporte.java
+++ java-incrementos-y-condicionales/src/Programas/Ejercicio3Transporte.java
@@ -26,13 +26,55 @@ public class Ejercicio3Transporte {
     
     public static void main(String[] args) {
         
-        String calificacion;
-        String cantviajes
+        double calificacion;
+        double cantViajes;
+        String disponible;
+        int condiciones=0;
+        
+        
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("");
+        System.out.print("Ingrese su calificacion: ");
+        calificacion=sc.nextDouble();
+        
+        System.out.print("Ingrese la cantidad de viajes realizados: ");
+        cantViajes=sc.nextDouble();
+        
+        sc.nextLine(); //esto lo puse porque me estaba leyendo vacia la entrada "disponoble" y se saltaba directamente a terminar el codigo, sin dar
+                       //tiempo al usuario que digite la respuesta.
+        
+        System.out.println("Se encuentra con disponibilidad? Si/No: ");
+        disponible=sc.nextLine();
+        
+        if(calificacion>=4.8){
+        
+           condiciones++;
+    }
+        if (cantViajes>500){
+          condiciones++;
+    }
+        if(disponible.equalsIgnoreCase("Si")){
+            
+          condiciones++;
         
     }
+        
+        if(condiciones==3){
+            System.out.println("Estimado conductor, usted a sido asignado con categoria Premium");
+        }
+        
+                else if(condiciones==2){
+            System.out.println("Estimado conductor, usted a sido asignado con categoria Estandar");
+        }
+        
+                else{
+                    System.out.println("No cumple con los requisitos para ser asignado con alguna categoria");
+                }
+        
+        
+        
+        
     
+    }
 }
